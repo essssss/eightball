790d07901cf1ebdb80d3439a94e80831a81b461d
Steven <srmcnamara7@gmail.com>
2023-08-24 20:51:32 -0400

    eighball

---
+++ src/EightBall.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./EightBall.css";
+
+const EightBall = () => {
+    const msgArray = [
+        { msg: "It is certain.", color: "green" },
+        { msg: "It is decidedly so.", color: "green" },
+        { msg: "Without a doubt.", color: "green" },
+        { msg: "Yes - definitely.", color: "green" },
+        { msg: "You may rely on it.", color: "green" },
+        { msg: "As I see it, yes.", color: "green" },
+        { msg: "Most likely.", color: "green" },
+        { msg: "Outlook good.", color: "green" },
+        { msg: "Yes.", color: "green" },
+        { msg: "Signs point to yes.", color: "goldenrod" },
+        { msg: "Reply hazy, try again.", color: "goldenrod" },
+        { msg: "Ask again later.", color: "goldenrod" },
+        { msg: "Better not tell you now.", color: "goldenrod" },
+        { msg: "Cannot predict now.", color: "goldenrod" },
+        { msg: "Concentrate and ask again.", color: "goldenrod" },
+        { msg: "Don't count on it.", color: "red" },
+        { msg: "My reply is no.", color: "red" },
+        { msg: "My sources say no.", color: "red" },
+        { msg: "Outlook not so good.", color: "red" },
+        { msg: "Very doubtful.", color: "red" },
+    ];
+    const genAnswer = () =>
+        msgArray[Math.floor(Math.random() * msgArray.length)];
+    const askQuestion = () => {
+        let newAnswer = genAnswer();
+        console.log(newAnswer);
+        setMsg(newAnswer.msg);
+        setColor(newAnswer.color);
+    };
+    const [msg, setMsg] = useState("Think of a Question");
+    const [color, setColor] = useState("black");
+
+    return (
+        <div className={`EightBall ${color}`} onClick={askQuestion}>
+            <p className="EightBall-text">{msg}</p>
+        </div>
+    );
+};
+
+export default EightBall;
